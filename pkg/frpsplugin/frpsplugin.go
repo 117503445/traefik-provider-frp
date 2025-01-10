41e4b2ac192bc6cafd9d59697fe0aa479ba933ad
117503445 <t117503445@gmail.com>
2025-01-10 17:46:40 +0800

    frpsplugin
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- pkg/frpsplugin/frpsplugin.go
+++ pkg/frpsplugin/frpsplugin.go
@@ -2,6 +2,7 @@ package frpsplugin
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/rs/zerolog/log"
@@ -12,7 +13,22 @@ type Server struct {
 
 func (s *Server) Serve(port int) error {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		var err error
+		// get args['op']
+		op := r.URL.Query().Get("op")
+
+		payload, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to read request body")
+			return
+		}
+
+		log.Info().Str("op", op).Str("payload", string(payload)).Msg("req")
+
+		_, err = w.Write([]byte(`{"reject": false,"unchange": true}`))
+		if err != nil {
+			log.Error().Err(err).Msg("failed to write response")
+		}
 	})
 	log.Info().Int("port", port).Msg("start frpsplugin server")
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
